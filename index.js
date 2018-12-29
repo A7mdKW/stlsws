f81e1ff5de82c2c89a31ba08cba69936e8baa10f
A7mdKW <43846712+A7mdKW@users.noreply.github.com>
2018-12-29 23:18:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '+'
-client.on('message', message => {
-  if (!message.content.startsWith(prefix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-if (message.content.startsWith(prefix + 'playing')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult}** : Status changed`)
-} else
-if (message.content.startsWith(prefix + 'Stream')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/ChampionBot");
-    message.channel.sendMessage(`**${argresult}** :The bot stream has been changed`)
-} else
-if (message.content.startsWith(prefix + 'watching')) {
-  if (message.author.id !== '351472610732670976') return message.reply('**only for the botowner **')
-    client.user.setActivity(argresult, {type : 'watching'});
- message.channel.sendMessage(`**${argresult}** : Watching changed to`)
- message.react("👌")
- 
- if (message.content.startsWith(prefix + 'listening')) {
-  client.user.setActivity(argresult, {type: 'LISTENING'})
-     console.log('test' + argresult);
-    message.channel.sendMessage(`LISTENING Now: **${argresult}`)
-} 
-}
-});
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag} !`);
+          client.user.setActivity("you",{type: 'WATCHING'});
+  
+  });
 
 
 client.login(process.env.USER_TOKEN);
