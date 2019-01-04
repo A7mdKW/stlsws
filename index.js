3258d7eaf6310902e0d572618bf6426fc51a5fcc
A7mdKW <43846712+A7mdKW@users.noreply.github.com>
2019-01-04 13:30:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '+'
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag} !`);
-          client.user.setActivity("you",{type: 'WATCHING'});
+client.on('message', message => {
+  if (!message.content.startsWith(PREFIX)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== "351472610732670976") return;
+
   
-  });
+  if (message.content.startsWith(prefix + 'setwatching')) {
+  client.user.setActivity(argresult, {type: 'WATCHING'})
+     console.log('test' + argresult);
+    message.channel.sendMessage(`**Watch Now: **${argresult}`)
+} 
+
+ 
+  if (message.content.startsWith(prefix + 'setlistening')) {
+  client.user.setActivity(argresult, {type: 'LISTENING'})
+     console.log('test' + argresult);
+    message.channel.sendMessage(`LISTENING Now: **${argresult}`)
+} 
+
+
+if (message.content.startsWith(prefix + 'setstream')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/KiNg66S");
+     console.log('test' + argresult);
+    message.channel.sendMessage(`**Streaming: **${argresult}`)
+} 
+});
 
 
 client.login(process.env.USER_TOKEN);
