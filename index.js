87720209189e9b850fc967cde185e149d012aa68
A7mdKW <43846712+A7mdKW@users.noreply.github.com>
2019-01-10 01:13:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const client = new Discord.Client();
 const prefix = '+'
 
 
-client.on('ready', () => {
+/*client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag} !`);
           client.user.setActivity("You",{type: 'WATCHING'});
   
   });
 
 
-client.login(process.env.USER_TOKEN);
+client.login(process.env.USER_TOKEN);*/
